69bd48b8814fb187caf2188779332abe6c51ba18
Maxim <mlukagon@mail.ru>
2025-05-05 18:41:44 +0400

    triangle(recall shaders and etc)

--- 3DEngineCore/src/3DEngineCore/Window.cpp
+++ 3DEngineCore/src/3DEngineCore/Window.cpp
@@ -6,11 +6,50 @@
 #include <imgui/backends/imgui_impl_opengl3.h>
 #include <imgui/backends/imgui_impl_glfw.h>
 
+
+
+    GLfloat point[] = {
+     0.0f, 0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     -0.5f, -0.5f, 0.0f
+    };
+
+    GLfloat colors[] = { 
+      1.0f, 0.0f, 0.0f,
+      0.0f, 1.0f, 0.0f,
+      0.0f, 0.0f, 1.0f
+    };
+
+    const char* vertex_shader =   
+        "#version 460\n"
+        "layout(location = 0) in vec3 vertexCoords;"
+        "layout(location = 1) in vec3 vertexColor;"
+        "out vec3 frag_color;"
+        "void main()"
+        "{"
+          "frag_color = vertexColor;"
+          "gl_Position = vec4(vertexCoords,1.0);"
+        "}";
+   
+
+    const char* fragment_shader =
+        "#version 460\n"
+        "in vec3 frag_color;"
+        "out vec4 color;"
+        "void main() {"
+        "color = vec4(frag_color, 1.0);"
+        "}";
+    
+
+    GLint idProgram = 0;
+    GLuint vao = 0;
 bool Engine::Window::is_GLFW_initialized = false;
 Engine::Window::Window(unsigned int&& width, unsigned int&& height, const char* title)
 	:  m_window(nullptr)
     , data({ std::move(width), std::move(height), title })
-{}
+{
+  /*  glViewport(0, 0, getWidth(), getHeight());*/
+}
 
 int Engine::Window::createWindow()
 {
@@ -57,12 +96,53 @@ int Engine::Window::createWindow()
             return 0;
         }
 
+      
         IMGUI_CHECKVERSION();
         ImGui::CreateContext(); 
         ImGui_ImplOpenGL3_Init();
         ImGui_ImplGlfw_InitForOpenGL(m_window, true);
+
+        GLint idVertex = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(idVertex, 1, &vertex_shader, nullptr);
+        glCompileShader(idVertex);
+
+        GLint idFragment = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(idFragment, 1, &fragment_shader, nullptr);
+        glCompileShader(idFragment);
+
+        idProgram = glCreateProgram();
+        glAttachShader(idProgram, idVertex);
+        glAttachShader(idProgram, idFragment);
+        glLinkProgram(idProgram);
+
+        glDeleteShader(idVertex);
+        glDeleteShader(idFragment);
+
+        vao = 0;
+        glGenVertexArrays(1, &vao);
+        glBindVertexArray(vao);
+
+        GLuint points_vbo;
+        glGenBuffers(1, &points_vbo); // отличие от glCreateBuffers
+        glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(point), point, GL_STATIC_DRAW);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+        GLuint colors_vbo;
+        glGenBuffers(1, &colors_vbo);
+        glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+        glBindVertexArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+
         return 0;
 
+     
+       
 }
 
 void Engine::Window::makeContext()
@@ -94,6 +174,9 @@ void Engine::Window::on_update()
         glClearColor(m_backGroundColor[0], m_backGroundColor[1], m_backGroundColor[2], m_backGroundColor[3]);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glBindVertexArray(vao);
+        glUseProgram(idProgram);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
       
         ImGuiIO& io = ImGui::GetIO(); // получили handle
         io.DisplaySize.x = static_cast<float>(getWidth());
@@ -107,9 +190,11 @@ void Engine::Window::on_update()
         ImGui::ColorEdit4("BackGround Color", m_backGroundColor);
         ImGui::End();
 
+      
 
         ImGui::Render();// создает данные
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());//рисует посредством опенгл
+        
 
         swapBuffers();
         glfwPollEvents();
